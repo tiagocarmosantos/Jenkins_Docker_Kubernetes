43722082d9f6a1a1d64a4f004351f1a7505d59e3
Tiago Carmo Santos <tiagocarmosantos@hotmail.com>
2021-04-26 13:48:48 -0300

    Learning and Training - Jenkins em larga escala via Docker e Kubernetes

---
+++ recursos-v0-v1/v.0.3.0/jenkins_home/.bash_history
@@ -0,0 +1,68 @@
+ls
+exit
+ls
+cd /srv/backup
+ls
+cd FULL-2021-04-05_15-30/
+LS
+ls
+cls
+cls
+clear
+ls
+cd ..
+ls
+clear
+ls
+ls
+ls
+exit
+ls
+cd /srv/backup
+ls
+cd /srv/backup
+ls
+ls
+cd ..
+ls
+cd ..
+ls
+cd /srv/backup
+ls
+cls
+clera
+clera
+clear
+cd ..
+cd ..
+ls
+exit
+LS
+ls
+cd /srv/backup
+ls
+cd ..
+cd ..
+cd /srv/backup
+ls
+cd /srv/backup
+ls
+ls
+ls
+ls
+ls
+ls
+exit
+ls
+cd /srv/backup
+ls
+ls
+cp BACKUPSET_2021-04-05_18-42_.zip .
+ls
+EXIT
+exit
+ls
+cd /srv/backup
+ls
+cp BACKUPSET_2021-04-05_18-42_.zip .
+exit
---
+++ recursos-v2/v.2.0.0/setup-and-run-rodrigogrohl.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+dockerhub_user=rodrigogrohl
+
+jenkins_port=8080
+image_name=missao-devops-jenkins
+image_version=2.0.0
+container_name=md-jenkins
+
+docker stop ${container_name}
+
+docker build -t ${dockerhub_user}/${image_name}:${image_version} .
+
+if [ ! -d m2deps ]; then
+    mkdir m2deps
+fi
+if [ -d jobs ]; then
+    rm -rf jobs
+fi
+if [ ! -d jobs ]; then
+    mkdir jobs
+fi
+
+docker run -p ${jenkins_port}:8080 \
+    -v $(pwd)/downloads:/var/jenkins_home/downloads \
+    -v $(pwd)/jobs:/var/jenkins_home/jobs/ \
+    -v $(pwd)/m2deps:/var/jenkins_home/.m2/repository/ \
+    -v $HOME/.ssh:/var/jenkins_home/.ssh/ \
+    --rm --name ${container_name} \
+    ${dockerhub_user}/${image_name}:${image_version}
